e582bb24857ca3a7dab9a636a2e547602c29223a
Bhakti Sanas <yourgithubemail@gmail.com>
2026-07-17 20:13:40 +0530

    Configure FastAPI project structure and MongoDB connection

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemes.user_schema import UserSignup
 router = APIRouter()
 
 @router.post("/signup")
 def signup(user: UserSignup):
+
+    # Check if passwords match
+    if user.password != user.confirm_password:
+        raise HTTPException(
+            status_code=400,
+            detail="Passwords do not match."
+        )
+
     return {
-        "message": "Signup API is working!",
-        "user": user
+        "message": "Passwords matched successfully!"
     }
