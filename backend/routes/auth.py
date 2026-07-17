b3d3c5540afa6d111bfb48c96eaaec2f842a8c57
Bhakti Sanas <yourgithubemail@gmail.com>
2026-07-17 20:48:31 +0530

    Implement user signup validation and MongoDB registration

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,17 +1,52 @@
 from fastapi import APIRouter, HTTPException
 from schemes.user_schema import UserSignup
+from database import db
+
 router = APIRouter()
 
 @router.post("/signup")
 def signup(user: UserSignup):
 
-    # Check if passwords match
+    # Check if password and confirm password are the same
     if user.password != user.confirm_password:
         raise HTTPException(
             status_code=400,
             detail="Passwords do not match."
         )
 
+    # Check if username already exists
+    existing_username = db.users.find_one({
+        "username": user.username
+    })
+
+    if existing_username:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already exists."
+        ) 
+    
+    existing_email = db.users.find_one({
+    "email": user.email
+    })
+
+    if existing_email:
+     raise HTTPException(
+        status_code=400,
+        detail="Email already exists."
+    )
+
+    # Create user document
+    user_data = {
+        "name": user.name,
+        "username": user.username,
+        "email": user.email,
+        "phone": user.phone,
+        "password": user.password
+    }
+
+    # Save user
+    db.users.insert_one(user_data)
+
     return {
-        "message": "Passwords matched successfully!"
+        "message": "User registered successfully!"
     }
